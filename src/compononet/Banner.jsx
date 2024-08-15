55fc7ea0f04b64e59b311cb70ed50231f288974e
abdallaafifi <abdallaafifi@gmail.com>
2024-08-15 07:35:35 +0300

    nn

--- src/compononet/Banner.jsx
+++ src/compononet/Banner.jsx
@@ -3,15 +3,15 @@ import React from "react";
 const Banner = () => {
   const Data = [
     {
-      img: "src/public/assets/images/viewers-disney.png",
+      img: "./public/assets/images/viewers-disney.png",
       video: "/src/public/assets/videos/1564674844-disney.mp4",
     },
     {
-      img: "src/public/assets/images/viewers-marvel.png",
+      img: "./src/public/assets/images/viewers-marvel.png",
       video: "/src/public/assets/videos/1564676115-marvel.mp4",
     },
     {
-      img: "src/public/assets/images/viewers-national.png",
+      img: "./public/assets/images/viewers-national.png",
       video: "/src/public/assets/videos/1564676296-national-geographic.mp4",
     },
     {
@@ -19,12 +19,12 @@ const Banner = () => {
       video: "/src/public/assets/videos/1564676714-pixar.mp4",
     },
     {
-      img: "src/public/assets/images/viewers-starwars.png",
+      img: "/public/assets/images/viewers-starwars.png",
       video: "/src/public/assets/videos/1608229455-star-wars.mp4",
     },
   ];
   return (
-    <div className="w-full mt-4  mb-4 flex items-center flex-wrap md:gap-0 gap-8 px-2 md:px-8 justify-center md:justify-between">
+    <div className="w-full mt-4  mb-4 flex items-center flex-wrap md:gap-12 gap-8 px-2 md:px-8 justify-center md:justify-between">
       {Data.map((item, i) => (
         <div
           key={i}
